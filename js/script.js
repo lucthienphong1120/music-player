6622a3c2311d457964a8ea3e6462ec4e54ffc849
Lục Thiên Phong <90561566+lucthienphong1120@users.noreply.github.com>
2022-01-12 12:37:21 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,523 @@
+var songs = {
+    us: [
+        {
+            name: 'Lily',
+            singer: 'Alan Walker',
+            path: './assets/music/us/Lily.mp3',
+            image: './assets/img/us/lily.jpg'
+        },
+        {
+            name: 'Lost control',
+            singer: 'Alan Walker',
+            path: './assets/music/us/Lost Control.mp3',
+            image: './assets/img/us/lost-control.jpg'
+        },
+        {
+            name: 'Hymn for the weekend',
+            singer: 'Coldplay',
+            path: './assets/music/us/Hymn For The Weekend.mp3',
+            image: './assets/img/us/Coldplay,_Hymn_for_the_Weekend,_Artwork.jpg'
+        },
+        {
+            name: 'On my way',
+            singer: 'Alan Walker',
+            path: './assets/music/us/On My Way.mp3',
+            image: './assets/img/us/on-my-way.jpg'
+        },
+        {
+            name: 'Play date',
+            singer: 'Melanie Martinez',
+            path: './assets/music/us/Play Date.mp3',
+            image: './assets/img/us/play-date.jpg'
+        },
+        {
+            name: 'The Nights',
+            singer: 'Avicii',
+            path: './assets/music/us/The Nights.mp3',
+            image: './assets/img/us/the-night.jpg'
+        },
+        {
+            name: 'River',
+            singer: 'Charlie Puth',
+            path: './assets/music/us/River Official Audio.mp3',
+            image: './assets/img/us/river.jpg'
+        },
+        {
+            name: 'Waiting for love',
+            singer: 'Avicii',
+            path: './assets/music/us/Waiting For Love.mp3',
+            image: './assets/img/us/waiting-for-love.jpg'
+        },
+        {
+            name: 'Breathless',
+            singer: 'Shayne Ward',
+            path: './assets/music/us/Breathless - Shayne Ward.mp3',
+            image: './assets/img/us/song2.png'
+        },
+        {
+            name: 'No Promises',
+            singer: 'Shayne Ward',
+            path: './assets/music/us/No Promises - Shayne Ward.mp3',
+            image: './assets/img/us/song3.png'
+        },
+        {
+            name: 'Until You',
+            singer: 'Shayne Ward',
+            path: './assets/music/us/Until You - Shayne Ward.mp3',
+            image: './assets/img/us/song7.png'
+        },
+    ],
+    vn: [
+        {
+            name: 'Tháng mấy em nhớ anh',
+            singer: 'Hà Anh Tuấn',
+            path: './assets/music/vn/thang_may_em_nho_anh.mp3',
+            image: './assets/img/vn/thang-may-em-nho-an.jpg'
+        },
+        {
+            name: 'Bài này chill phết',
+            singer: 'Đen Vâu',
+            path: './assets/music/vn/Bài Này Chill Phết.mp3',
+            image: './assets/img/vn/bainaychillphet.jpg'
+        },
+        {
+            name: 'Nơi này có anh',
+            singer: 'Sơn Tùng',
+            path: './assets/music/vn/NƠI NÀY CÓ ANH.mp3',
+            image: './assets/img/vn/Nơi_này_có_anh.jpg'
+        },
+        {
+            name: 'Chỉ là không cùng nhau',
+            singer: 'Tăng Phúc',
+            path: './assets/music/vn/CHỈ LÀ KHÔNG CÙNG NHAU.mp3',
+            image: './assets/img/vn/chi-la-khong-cung-nhau.jpg'
+        },
+        {
+            name: 'Sứ Thanh Hoa',
+            singer: 'Jay Chou',
+            path: './assets/music/vn/SuThanhHoa-JayChou_5nhf.mp3',
+            image: './assets/img/vn/song6.png'
+        },
+        {
+            name: 'Lối nhỏ',
+            singer: 'Đen Vâu',
+            path: './assets/music/vn/Lối Nhỏ.mp3',
+            image: './assets/img/vn/loinho.jpg'
+        },
+        {
+            name: 'Thanh Xuân',
+            singer: 'Da LAB',
+            path: './assets/music/vn/Thanh Xuân.mp3',
+            image: './assets/img/vn/thanhxuan.jpg'
+        },
+        {
+            name: 'Thiên Đàng',
+            singer: 'Joli Poli - Wowy',
+            path: './assets/music/vn/THIÊN ĐÀNG.mp3',
+            image: './assets/img/vn/thiendang.jpg'
+        },
+        {
+            name: 'Tất cả sẽ thay anh',
+            singer: 'Tăng Phúc',
+            path: './assets/music/vn/Tất Cả Sẽ Thay Anh.mp3',
+            image: './assets/img/vn/tatcasethayanh.jpg'
+        },
+        {
+            name: 'Đừng chờ anh nữa',
+            singer: 'Tăng Phúc',
+            path: './assets/music/vn/ĐỪNG CHỜ ANH NỮA.mp3',
+            image: './assets/img/vn/dungchoanhnua.jpg'
+        },
+        {
+            name: 'Đài Hoa Cúc',
+            singer: 'Jay Chou',
+            path: './assets/music/vn/Dai Hoa Cuc - Jay Chou.mp3',
+            image: './assets/img/vn/song4.png'
+        },
+        {
+            name: 'Hương Lúa',
+            singer: 'Jay Chou',
+            path: './assets/music/vn/HuongLua-ChauKietLuan_32rfj.mp3',
+            image: './assets/img/vn/song5.png'
+        }
+    ],
+    love: []
+}
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const USER_STORAGE = 'user activity';
+const MUSIC_LOVE = 'love songs';
+
+const player = $('.player');
+const heading = $('header h2');
+const cdThumb = $('.cd-thumb');
+const audio = $('#audio');
+const cd = $('.cd');
+const playList = $('.playlist');
+const timeStart = $('.timing-left');
+const timeEnd = $('.timing-right');
+const timeProgress = $('#time-progress');
+const volumeProgress = $('#volume-progress');
+const volumeBtn = $('.volume-btn');
+const volumeUpBtn = $('.volume-btn i:first-child');
+const volumeMuteBtn = $('.volume-btn i:last-child');
+const playBtn = $('.btn-toggle-play');
+const nextBtn = $('.btn-next');
+const prevBtn = $('.btn-prev');
+const randomBtn = $('.btn-random');
+const repeatBtn = $('.btn-repeat');
+const vnBtn = $('.vn-song-btn');
+const usBtn = $('.us-song-btn');
+const loveBtn = $('.love-song-btn');
+const addLove = $('.add-love');
+const addLoveText = $('.add-love-text');
+const overlay = $('.overlay');
+
+let randomArray = [];
+const app = {
+    currentIndex: 0,
+    currentList: 'us',
+    isPlaying: false,
+    isRandom: false,
+    isRepeat: false,
+    isMuted: false,
+    config: JSON.parse(localStorage.getItem(USER_STORAGE)) || {},
+    configLove: JSON.parse(localStorage.getItem(MUSIC_LOVE)) || [],
+    // set config
+    setConfig(key, value) {
+        this.config[key] = value;
+        localStorage.setItem(USER_STORAGE, JSON.stringify(this.config));
+    },
+    setConfigLove: function (arrayLove) {
+        this.configLove = arrayLove;
+        localStorage.setItem(MUSIC_LOVE, JSON.stringify(this.configLove))
+    },
+    songs,
+    // render the song list
+    render() {
+        const htmls = this.songs[this.currentList].map((song, index) => {
+            return `
+                <div data-index="${index}" class="song${index == this.currentIndex ? ' active' : ''}">
+                    <div class="thumb"
+                        style="background-image: url('${song.image}')">
+                    </div>
+                    <div class="body">
+                        <h3 class="title">${song.name}</h3>
+                        <p class="author">${song.singer}</p>
+                    </div>
+                    <div class="option">
+                        <i class="fas fa-ellipsis-h"></i>
+                    </div>
+                </div>
+            `
+        })
+        playList.innerHTML = htmls.join('');
+    },
+    // define properties of song
+    defineProperties() {
+        Object.defineProperty(this, 'currentSong', {
+            get() {
+                return this.songs[this.currentList][this.currentIndex];
+            }
+        })
+    },
+    // handle event and song
+    handleEvent() {
+        // handle rotate cd
+        const cdAnimate = cdThumb.animate([
+            { transform: 'rotate(360deg)' }
+        ], {
+            duration: 20000,
+            iterations: Infinity
+        });
+        cdAnimate.pause();
+        // handle when scroll the song list
+        const cdWidth = cd.offsetWidth;
+        document.onscroll = function () {
+            const scrollTop = document.documentElement.scrollTop || window.scrollY;
+            const newCdWitdh = cdWidth - scrollTop;
+            cd.style.width = newCdWitdh > 0 ? newCdWitdh + 'px' : 0;
+            cd.style.opacity = newCdWitdh / cdWidth;
+        }
+        // handle when click button
+        playBtn.onclick = () => {
+            if (this.isPlaying) {
+                audio.pause();
+            } else {
+                audio.play();
+            }
+        }
+        // handle when playing
+        audio.onplay = () => {
+            this.isPlaying = true;
+            player.classList.add('playing');
+            cdAnimate.play();
+        }
+        // handle when pause
+        audio.onpause = () => {
+            this.isPlaying = false;
+            player.classList.remove('playing');
+            cdAnimate.pause();
+        }
+        // handle time of song in progress bar
+        audio.ontimeupdate = () => {
+            if (audio.duration) {
+                // handle current time
+                const percent = Math.floor(audio.currentTime / audio.duration * 100);
+                timeProgress.value = percent;
+                // time start
+                const currentStartTime = Math.floor(audio.currentTime);
+                const currentStartSeconds = currentStartTime % 60;
+                const currentStartMinutes = Math.floor(currentStartTime / 60);
+                timeStart.textContent = String(currentStartMinutes).padStart(2, '0') + ":" + String(currentStartSeconds).padStart(2, '0');
+                // time end
+                const currentEndTime = Math.floor(audio.duration);
+                const currentEndSeconds = currentEndTime % 60;
+                const currentEndMinutes = Math.floor(currentEndTime / 60);
+                timeEnd.textContent = String(currentEndMinutes).padStart(2, '0') + ":" + String(currentEndSeconds).padStart(2, '0');
+            }
+        }
+        // handle when song ended
+        audio.onended = () => {
+            if (this.isRepeat) {
+                setTimeout(() => {
+                    audio.play();
+                }, 500);
+            }
+            else {
+                setTimeout(() => {
+                    nextBtn.click();
+                }, 500);
+            }
+        }
+        // handle when click the progress bar
+        timeProgress.oninput = (e) => {
+            const seekTime = e.target.value / 100 * audio.duration;
+            audio.currentTime = seekTime;
+        }
+        // handle when click the volume bar
+        volumeProgress.oninput = (e) => {
+            audio.volume = e.target.value / 100;
+            volumeProgress.value = audio.volume * 100;
+        }
+        // handle when click next button
+        nextBtn.onclick = () => {
+            if (this.isRandom) {
+                this.randomSong();
+            }
+            else {
+                this.nextSong();
+            }
+            audio.play();
+            this.handleActiveSong();
+            cdAnimate.cancel();
+        }
+        // handle when click prev button
+        prevBtn.onclick = () => {
+            if (this.isRandom) {
+                this.randomSong();
+            }
+            else {
+                this.prevSong();
+            }
+            audio.play();
+            this.handleActiveSong();
+            cdAnimate.cancel();
+        }
+        // handle when click random button
+        randomBtn.onclick = () => {
+            this.isRandom = !this.isRandom;
+            this.setConfig('isRandom', this.isRandom);
+            randomBtn.classList.toggle('active');
+        }
+        // handle when click repeat button
+        repeatBtn.onclick = () => {
+            this.isRepeat = !this.isRepeat;
+            this.setConfig('isRepeat', this.isRepeat);
+            repeatBtn.classList.toggle('active');
+        }
+        // Handle Mute & UnMute
+        volumeBtn.onclick = () => {
+            this.isMuted = !this.isMuted;
+            audio.volume = !audio.volume;
+            volumeUpBtn.classList.toggle('active');
+            volumeMuteBtn.classList.toggle('active');
+            volumeProgress.value = String(audio.volume * 100);
+        };
+        // handle when click overlay
+        overlay.onclick = () => {
+            overlay.classList.remove('active');
+            addLove.classList.remove('active');
+        }
+        // hanle when change list
+        vnBtn.onclick = () => {
+            addLoveText.innerText = 'Thêm vào danh sách bài hát yêu thích';
+            $('.catagory.active').classList.remove('active');
+            vnBtn.classList.add('active');
+            this.currentList = 'vn';
+            this.handleSwitchList();
+            cdAnimate.cancel();
+        }
+        usBtn.onclick = () => {
+            addLoveText.innerText = 'Thêm vào danh sách bài hát yêu thích';
+            $('.catagory.active').classList.remove('active');
+            usBtn.classList.add('active');
+            this.currentList = 'us';
+            this.handleSwitchList();
+            cdAnimate.cancel();
+        }
+        loveBtn.onclick = () => {
+            if (this.songs['love'].length == 0) {
+                playList.innerHTML = `<div class="empty-list">Bạn chưa có bài hát yêu thích</div>`;
+            }
+            else {
+                addLoveText.innerText = 'Xóa bài hát khỏi mục yêu thích';
+                $('.catagory.active').classList.remove('active');
+                loveBtn.classList.add('active')
+                this.currentList = 'love';
+                this.handleSwitchList();
+                cdAnimate.cancel();
+            }
+        }
+        // handle when click a song
+        playList.onclick = (e) => {
+            const songClicked = e.target.closest('.song')
+            const songItem = e.target.closest('.song:not(.active)');
+            const optionBtn = e.target.closest('.option');
+            // option button
+            if (optionBtn) {
+                addLove.classList.add('active');
+                overlay.classList.add('active');
+                addLoveText.onclick = () => {
+                    // remove from love list
+                    if (this.currentList == 'love') {
+                        var songIndex = songClicked.getAttribute('data-index');
+                        var deleteLove = this.songs['love'][songIndex];
+                        this.songs['love'].splice(songIndex, 1);
+                        this.render();
+                        this.setConfigLove(this.songs['love']);
+                        alert(`bạn đã xóa bài hát "${deleteLove.name}" khỏi mục yêu thích`);
+                        addLove.classList.remove('active');
+                        overlay.classList.remove('active');
+                        if (this.songs['love'].length == 0) {
+                            playList.innerHTML = `<div class="empty-list">Không phải bug, đây là tính năng</div>`;
+                        }
+                    }
+                    // add song to love list
+                    else {
+                        var songIndex = songClicked.getAttribute('data-index');
+                        if (this.currentList == 'us') {
+                            var newLove = this.songs['us'][songIndex];
+                        }
+                        else if (this.currentList == 'vn') {
+                            var newLove = this.songs['vn'][songIndex];
+                        }
+                        this.setConfigLove(newLove);
+                        if (this.songs['love'].includes(newLove)) {
+                            alert('Bài hát này đã có trong mục yêu thích');
+                        }
+                        else {
+                            this.songs['love'].push(newLove);
+                            this.setConfigLove(this.songs['love']);
+                            alert(`bạn đã thêm bài hát "${newLove.name}" vào mục yêu thích`);
+                        }
+                        addLove.classList.remove('active');
+                        overlay.classList.remove('active');
+                    }
+                }
+            }
+            // change song
+            else if (songItem) {
+                this.currentIndex = songItem.dataset.index;
+                this.loadSong();
+                audio.play();
+                this.handleActiveSong();
+                cdAnimate.cancel();
+            }
+        }
+    },
+    // load the song
+    loadSong() {
+        heading.innerText = this.currentSong.name;
+        cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
+        audio.src = this.currentSong.path;
+        timeStart.textContent = '00:00';
+        timeEnd.textContent = '00:00';
+    },
+    loadConfig() {
+        Object.assign(this, this.config);
+        if (this.isRandom) {
+            randomBtn.classList.add('active');
+        }
+        if (this.isRepeat) {
+            repeatBtn.classList.add('active');
+        }
+        if (this.configLove.length > 0) {
+            this.songs['love'] = this.songs['love'].concat(this.configLove);
+        }
+    },
+    // handle active current song
+    handleActiveSong() {
+        const songsList = $$('.song');
+        $('.song.active').classList.remove('active');
+        const songActive = songsList[this.currentIndex];
+        songActive.classList.add('active')
+        songActive.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
+    },
+    // handle switch playlist
+    handleSwitchList() {
+        this.currentIndex = 0;
+        this.loadSong();
+        this.render();
+        audio.play();
+        this.handleActiveSong();
+    },
+    // handle when next song
+    nextSong() {
+        this.currentIndex++;
+        if (this.currentIndex > this.songs[this.currentList].length - 1) {
+            this.currentIndex = 0;
+        }
+        this.loadSong();
+    },
+    // handle when prev song
+    prevSong() {
+        this.currentIndex--;
+        if (this.currentIndex < 0) {
+            this.currentIndex = this.songs[this.currentList].length - 1;
+        }
+        this.loadSong();
+    },
+    // handle when random song
+    randomSong() {
+        let randomIndex;
+        do {
+            randomIndex = Math.floor(Math.random() * this.songs[this.currentList].length);
+            // handle no reapeat song
+            if (randomArray.length === this.songs[this.currentList].length) {
+                randomArray.length = 0;
+            }
+        } while (randomArray.includes(randomIndex));
+        randomArray.push(randomIndex);
+        this.currentIndex = randomIndex;
+        this.loadSong();
+    },
+    start() {
+        this.loadConfig();
+
+        this.defineProperties();
+
+        this.loadSong();
+
+        this.render();
+
+        this.handleEvent();
+    }
+}
+
+app.start();
